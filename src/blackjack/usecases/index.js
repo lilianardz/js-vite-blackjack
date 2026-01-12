ad4f8920fc8bb6d430425bd3113915556a2f30eb
lilianardz <lilianabergling@gmail.com>
2026-01-12 17:11:01 -0600

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+//archivo barril
+export { pedirCarta } from './pedir-carta';
+export { valorCarta } from './valor-carta';
+export {crearDeck} from './crear-deck';
+export {crearCartaHTML} from './crear-carta-html';
+export {turnoComputadora} from './turno-computadora';
